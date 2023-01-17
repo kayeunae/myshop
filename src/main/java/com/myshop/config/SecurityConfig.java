1757aa0634ff3b0050569f3e9ae1ef916f61d3f0
kayeunae <verte_ai@naver.com>
2023-01-17 20:39:35 +0900

    up

--- src/main/java/com/myshop/config/SecurityConfig.java
+++ src/main/java/com/myshop/config/SecurityConfig.java
@@ -32,7 +32,18 @@ public class SecurityConfig {
 			.logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))	//로그아웃 url
 			.logoutSuccessUrl("/");	//로그아웃 성공 시 이동할 url
 		
-		return http.build();
+		//페이지의 접근에 관한 설정
+		 http.authorizeRequests()
+		 .mvcMatchers("/css/**", "/js/**", "/img/**").permitAll()
+		 .mvcMatchers("/","/members/**","/item/**","/images/**").permitAll() //모든 사용자가 로그인(인증)없이 접근할 수 있도록 설정
+		 .mvcMatchers("/admin/**").hasRole("ADMIN") // '/admin'으로 시작하는 경로는 계정이 ADMIN role인 경우에만 접근 가능하도록 설정
+		 .anyRequest().authenticated();	//그 외 페이지는 모두 로그인(인증)을 받아야 한다.
+		
+		//인증되지 않은 사용자가 리소스(페이지, 이미지 등..)에 접근했을 때 설정
+		 http.exceptionHandling().authenticationEntryPoint(new CustomAuthenticationEntryPoint());
+		 
+		 
+		 return http.build();
 	}
 	
 	@Bean	//스프링에서 쓰이는 객체(객체를 리턴해주는 역할, 이걸 쓰니까 메소드에 리턴 값으로 객체를 받을 수 있게 됨.)
--- src/main/java/com/myshop/controller/MemberController.java
+++ src/main/java/com/myshop/controller/MemberController.java
@@ -1,5 +1,8 @@
 package com.myshop.controller;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -9,6 +12,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.myshop.dto.MemberFormDto;
 import com.myshop.entity.Member;
@@ -58,5 +62,41 @@ public class MemberController {
 		//이걸 하지 않으면 get 방식으로 하기 때문에 새로고침하거나 할 때 데이터가 또 들어갈 수 있기 때문에.
 		//리다이렉트는 새로고침을 하게 되면 안에 들어있던 데이터를 전부 날리고 처음부터 새로 다시 하는 것 !
 		// redirect:/ 에서 '/'는 루트 경로! 바로 localhost로(메인으로) 간다.
+	}
+	
+	//로그인 화면
+	@GetMapping(value = "/login")
+	public String loginMember() {
+		return "member/memberLoginForm";
+	}
+	
+	//세션 매니저 생성
+	private final SessionManager sessionManager;
+	
+	/*
+	//쿠키, 세션 테스트 (로그인 기능 없이 값만 받아오는 메소드)
+	@PostMapping(value = "/login2")
+	public String loginMember2(HttpServletResponse response, HttpSession session, @RequestParam String email) {
+		//HttpSession session: 세션을 저장하고 관리할 수 있는 객체
+		//@RequestParam String email : input 창에서 name이 email로 되어있는 값을 가져온다.
+		System.out.println("email: " + email);
+		
+		//Cookie는 (key, value)로 저장된다.
+		Cookie idCookie = new Cookie("userCookieId", email); //쿠키 생성
+		response.addCookie(idCookie);	//브라우저에 쿠키 저장
+		System.out.println("Cookie: " + idCookie);
+		
+//		session.setAttribute("userSessionId2",email); //setAttribute를 통해 서버에 있던 session을 session객체에 저장한다.
+		sessionManager.createSession(email, response);
+		
+		return "member/memberLoginForm";
+	}
+	*/
+	
+	//로그인을 실패했을 때
+	@GetMapping(value = "/login/error")
+	public String loginError(Model model) {
+		model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요.");
+		return "member/memberLoginForm";
 	}
 }
